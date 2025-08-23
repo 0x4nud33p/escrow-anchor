7c52e05ed19d76ef63acb23856fb1f88ddd00e16
0x4nud33p <anudeepavula009@gmail.com>
2025-08-23 10:43:26 +0000

    chore:project init

--- middlewares/rateLImiter.js
+++
@@ -1,9 +0,0 @@
-import rateLimit from "express-rate-limit";
-
-const rateLimiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 100, // limit each IP to 100 requests per windowMs
-  message: { error: "Too many requests, please try again later." },
-});
-
-export { rateLimiter }
